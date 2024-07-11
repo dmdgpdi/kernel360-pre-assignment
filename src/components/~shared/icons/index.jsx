05cc7789d44b1e0c9910be2ad977b06dcb69c0be
dmdgpdi <dmdgpdi@naver.com>
2024-07-11 23:26:17 +0900

    feat: icon 컴포넌트 생성

---
+++ src/components/~shared/icons/index.jsx
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+import { Heart, Fuel, Disc, UsersRound } from 'lucide-react';
+import { fontSize, color } from 'theme';
+
+function Icon({ type, color = 'black', size = fontSize.xl, isFill = false }) {
+  switch (type) {
+    case 'heart': {
+      return <Heart size={size} color={color} fill={isFill ? color : 'none'} />;
+    }
+    case 'fuel': {
+      return <Fuel size={size} color={color} fill={isFill ? color : 'none'} />;
+    }
+    case 'disc': {
+      return <Disc size={size} color={color} fill={isFill ? color : 'none'} />;
+    }
+    case 'people': {
+      return (
+        <UsersRound size={size} color={color} fill={isFill ? color : 'none'} />
+      );
+    }
+    default: {
+      return null;
+    }
+  }
+}
+
+const IconWrapperButton = styled.button`
+  width: fit-content;
+  height: min-content;
+  background: none;
+  border: none;
+  padding: 0;
+  cursor: pointer;
+`;
+
+function HeartButton() {
+  return (
+    <IconWrapperButton>
+      <Icon
+        type={'heart'}
+        size={fontSize.xl}
+        color={color.alertRed}
+        isFill={true}
+      />
+    </IconWrapperButton>
+  );
+}
+
+export { Icon, IconWrapperButton, HeartButton };
