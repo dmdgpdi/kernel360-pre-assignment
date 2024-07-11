0338d4a925e980cc5f7d42f0033445dcb2cf6124
dmdgpdi <dmdgpdi@naver.com>
2024-07-11 23:26:05 +0900

    feat: ~shared파일에서 export

---
+++ src/components/~shared/index.js
@@ -0,0 +1,6 @@
+export { Gap } from 'components/~shared/Gap';
+export { Padding } from 'components/~shared/Padding';
+export { Button } from 'components/~shared/Button';
+export { Text } from 'components/~shared/Text';
+export { Icon, IconWrapperButton, HeartButton } from 'components/~shared/icons';
+export { Card } from 'components/~shared/card';
